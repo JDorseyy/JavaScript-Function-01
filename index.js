3d9f582739f804b340f8ed2b4117a5f2cf2e45e8
Meek Dorsey <meekdorsey@Meeks-MacBook-Air.local>
2024-07-30 23:02:15 -0500

    song complete

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("hello world");
+function refrain() {
+    console.log("Yeah, Glo! Stomp a lil' pussy ho with some shell toes");
+console.log("Yeah, Glo! Slappin' rap bitches and makin' bail, ho");
+console.log("Yeah, Glo! Two-tone Cartier match the nails, ho");
+console.log("Yeah, Glo! No competition, these bitches stale, ho");
+console.log("Yeah, Glo! Cuban on Cuban, my shit in layers, ho");
+console.log("Yeah, Glo! Say they get money, but I can't tell, ho");
+console.log("Yeah, Glo! Keep runnin' circles around these snail hoes");
+console.log("Yeah, Glo! And my shit bumpin' up in the jail, ho (on the gang)");
+}
+console.log("Yeah, run up on big Glo, you won't, I bet yo' ass be on the floor");
+console.log("Big Carbon hit like Mayweather, Tula come through and bow a ho");
+console.log("Got millions out that rap shit, they hate a bitch but they stole the flow");
+console.log("Got backend after backend comin', fuck 'em, I stole the show");
+console.log("Don't run up on that bitch, she a headcase, all she do is shoot");
+console.log("I signed my deal up on a PJ and hopped out a parachute");
+console.log("I'm geeked, don't ask me 'bout no more features, I might just stare at you");
+console.log("I'm geeked, don't ask me 'bout no more features, I might embarrass you");
+
+refrain();
+
+console.log("Big Glo, where you been at? Man, everywhere, I'm workin' hard");
+console.log("Maybach and G-Wagon trucks, got M&M's in my garage");
+console.log("Quick to crank up on a ho, you bitches better not get me started");
+console.log("Locked in with the gangstas and players, my folks gon' keep it solid");
+console.log("Everything I got on new, it look like I got renovated (ayy)");
+console.log("I'm cocky, bitch, when I was just humble they didn't appreciate it (fuck 'em)");
+console.log("Young wealthy and ratchet, I'm the hood bitches' motivation (stamp that)");
+console.log("On top of the world now, they tried to underestimate me");
+console.log("Get 'em Glo! Them bitches fraudulent, you know you the truth");
+console.log("Get 'em Glo! Stop overthinkin', these hoes can't fuck with you (period)");
+console.log("Get 'em Glo! Standin' on business in these Chanel shoes");
+console.log("Before I let a ho play with me, I'll go eat some jail food, on gang");
+
+refrain();
+
+console.log("Yeah, Glo!");
+console.log("Yeah, Glo!");
