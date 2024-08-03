4ebe74ba1aa641c7db5913474b4ac555c42eec48
Meek Dorsey <meekdorsey@Meeks-MacBook-Air.local>
2024-08-03 18:26:53 -0500

    added a return statement

---
+++ index-2.js
@@ -0,0 +1,48 @@
+console.log('Hello World 2');
+
+function refrain() {
+let lyrics2 = "";
+lyrics2 += "Yeah, Glo! Stomp a lil' pussy ho with some shell toes\n"
+lyrics2 += "Yeah, Glo! Slappin' rap bitches and makin' bail, ho\n"
+lyrics2 += "Yeah, Glo! Two-tone Cartier match the nails, ho\n"
+lyrics2 += "Yeah, Glo! No competition, these bitches stale, ho\n"
+lyrics2 += "Yeah, Glo! Cuban on Cuban, my shit in layers, ho\n"
+lyrics2 += "Yeah, Glo! Say they get money, but I can't tell, ho\n"
+lyrics2 += "Yeah, Glo! Keep runnin' circles around these snail hoes\n"
+lyrics2 += "Yeah, Glo! And my shit bumpin' up in the jail, ho (on the gang)\n"
+
+return lyrics2;
+}
+
+function song() {
+    let l2;
+    // Verse one
+console.log("Yeah, run up on big Glo, you won't, I bet yo' ass be on the floor");
+console.log("Big Carbon hit like Mayweather, Tula come through and bow a ho");
+console.log("Got millions out that rap shit, they hate a bitch but they stole the flow");
+console.log("Got backend after backend comin', fuck 'em, I stole the show");
+console.log("Don't run up on that bitch, she a headcase, all she do is shoot");
+console.log("I signed my deal up on a PJ and hopped out a parachute");
+console.log("I'm geeked, don't ask me 'bout no more features, I might just stare at you");
+console.log("I'm geeked, don't ask me 'bout no more features, I might embarrass you");
+
+l2 = refrain();
+console.log(l2);
+// Verse two
+console.log("Big Glo, where you been at? Man, everywhere, I'm workin' hard");
+console.log("Maybach and G-Wagon trucks, got M&M's in my garage");
+console.log("Quick to crank up on a ho, you bitches better not get me started");
+console.log("Locked in with the gangstas and players, my folks gon' keep it solid");
+console.log("Everything I got on new, it look like I got renovated (ayy)");
+console.log("I'm cocky, bitch, when I was just humble they didn't appreciate it (fuck 'em)");
+console.log("Young wealthy and ratchet, I'm the hood bitches' motivation (stamp that)");
+console.log("On top of the world now, they tried to underestimate me");
+console.log("Get 'em Glo! Them bitches fraudulent, you know you the truth");
+console.log("Get 'em Glo! Stop overthinkin', these hoes can't fuck with you (period)");
+console.log("Get 'em Glo! Standin' on business in these Chanel shoes");
+console.log("Before I let a ho play with me, I'll go eat some jail food, on gang");
+
+console.log(l2);
+}
+
+ song();
